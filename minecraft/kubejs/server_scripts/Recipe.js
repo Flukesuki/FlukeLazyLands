31d117f2cb437bd3aa5a7f1c72bbb4ed68f85a50
li_waiwai <2157995671a@gmail.com>
2026-06-12 23:46:15 +0800

    搓搓搓

---
+++ minecraft/kubejs/server_scripts/Recipe.js
@@ -0,0 +1,16 @@
+ServerEvents.recipes(event => {
+    event.shapeless('minecraft:cobblestone', [
+        'kubejs:flukesuki_pebble',
+        'kubejs:flukesuki_pebble',
+        'kubejs:flukesuki_pebble',
+        'kubejs:flukesuki_pebble'
+    ]);
+});
+ServerEvents.recipes(event => {
+    event.shapeless('minecraft:oak_sapling', [
+        'botania:living_root',
+        'botania:living_root',
+        'botania:living_root',
+        'botania:living_root'
+    ]);
+});
--- minecraft/kubejs/startup_scripts/items.js
+++ minecraft/kubejs/startup_scripts/items.js
@@ -1,6 +1,6 @@
 StartupEvents.registry("item", event => {
     event.create("flukesuki_pebble")
-        .displayName("§7石子")
+        .displayName("石子")
         .maxStackSize(64)
         .tooltip("§7一颗普通的小石子")
         .texture("kubejs:item/flukesuki_pebble");
